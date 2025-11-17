636390b70119b720b5b3ca2797b62ba933fd3682
mdnestor <mdnest01@gmail.com>
2025-11-16 20:03:08 -0500

    notes on normal subgroups

--- Algae/GroupTheory/NormalSubgroup.lean
+++ Algae/GroupTheory/NormalSubgroup.lean
@@ -5,6 +5,10 @@ variable {α: Type u}
 
 open Group
 
+-- The (left) conjugation by an element g ∈ G of a group is the map a ↦ gag⁻¹.
+-- This is a left action of the group on itself.
+-- (The map a ↦ g⁻¹ag is the right conjugation since it corresponds to a right action.)
+
 def Conjugate [Group α] (g: α): α → α :=
   λ a ↦ g + a + -g
 
@@ -24,12 +28,18 @@ def Conjugate.action [Group α]: Action α α := {
     rw [inv_unit, op_unit_left, op_unit_right]
 }
 
+-- A normal subgroup is one which is invariant under conjugation.
+
 class Group.normalSubgroup [G: Group α] (S: Set α) extends toSubgroup: G.sub S where
   conj_invariant: Conjugate.action.invariant_set S
+
+-- In a commutative group, conjugation is trivial since gag⁻¹ = agg⁻¹ = a.
 
 theorem CommGroup.conjugate_trivial [CommGroup α] (g: α): Conjugate g = Function.id := by
   funext
   rw [Conjugate, op_comm, ←op_assoc, op_inv_left, op_unit_left, Function.id]
+
+-- Thus in a commutative group, every subgroup is normal.
 
 theorem CommGroup.subgroup_normal [G: CommGroup α] {S: Set α} (h: G.sub S): G.normalSubgroup S := by
   constructor
@@ -37,8 +47,14 @@ theorem CommGroup.subgroup_normal [G: CommGroup α] {S: Set α} (h: G.sub S): G.
   simp [Conjugate.action, CommGroup.conjugate_trivial]
   exact hx
 
+
+
+-- Given an element g and set S, the (left) coset is defined g + S = {g + a | a ∈ S}.
+
 def Coset [Magma α] (g: α) (S: Set α): Set α :=
   Set.image (λ a ↦ op g a) S
+
+-- Given a set S, there is a relation that says a related to b if a + S = b + S.
 
 def Coset.relation [Magma α] (S: Set α): Endorelation α :=
   λ a b ↦ Coset a S = Coset b S
